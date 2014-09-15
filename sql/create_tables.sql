8ab158e2bc779ac3b87de7d66c6541f5ec03b6d7
joopeti <pyry.joona@helsinki.fi>
2014-09-15 11:38:49 +0300

    muokattiin .sql-tiedostoja

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Asiakas
 (
-	AsiakasID integer serial PRIMARY KEY,
+	AsiakasID serial PRIMARY KEY,
 	Nimimerkki VARCHAR(32) NOT NULL,
 	Salasana VARCHAR(128) NOT NULL,
 	Email VARCHAR(128) NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE Asiakas
 );
 CREATE TABLE Viesti
 (
-	ViestiID integer serial PRIMARY KEY,
+	ViestiID serial PRIMARY KEY,
 	LahettajaID integer references Asiakas(AsiakasID) NOT NULL,
 	VastaanottajaID integer references Asiakas(AsiakasID) NOT NULL,
 	Sisalto varchar(256) NOT NULL,
@@ -20,24 +20,24 @@ CREATE TABLE Viesti
 );
 CREATE TABLE Kiinnostus
 (	
-	KiinnostusID integer serial PRIMARY KEY,
+	KiinnostusID serial PRIMARY KEY,
 	Nimi varchar(64) NOT NULL,
 	AsiakasID integer references Asiakas(AsiakasID)
 );
 CREATE TABLE Julkinensivu
 (
-	JulkinenID integer serial PRIMARY KEY,
+	JulkinenID serial PRIMARY KEY,
 	OmistajaID integer references Asiakas(AsiakasID),
 	Raportoitu boolean
 );
 CREATE TABLE Salainensivu
 (
-	SalainenID integer serial PRIMARY KEY,
+	SalainenID serial PRIMARY KEY,
 	OmistajaID integer references Asiakas(AsiakasID)
 );
 CREATE TABLE SalaisetSivutNakevat
 (
-	NakyvyysID integer serial PRIMARY KEY,
+	NakyvyysID serial PRIMARY KEY,
 	SalainenID integer references Salainensivu(SalainenID),
 	AsiakasID integer references Asiakas(AsiakasID)
 );
