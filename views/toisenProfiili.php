4dce3d83045e77c4a8048abbfe13e5755124a885
joopeti <pyry.joona@helsinki.fi>
2014-10-13 01:08:21 +0300

    etusivulle lisää sälää

--- views/toisenProfiili.php
+++ views/toisenProfiili.php
@@ -2,7 +2,7 @@
     <h3><?php echo $data->kayttaja->getNimimerkki(); ?></h3>
     <div class="row">
         <div class="col-md-4">
-            <p class="centeredImage"><img src="http://www.fact.co.uk/media/497038/the_shining_2.png" width="320" height="200" align="middle" alt="Kaunis nainen :D"></p>
+            <p class="centeredImage"><img src="pics/profile-placeholder.jpg" width="300" height="240" align="middle" alt="Kaunis nainen :D"></p>
             <div class="infovalikko">
                 <?php //if($_SESSION['admin'] || $_SESSION['kirjautunut'] == $data->kayttaja->getAsiakasID){ ?>
                 <a href="#">Raportoi</a><br>
